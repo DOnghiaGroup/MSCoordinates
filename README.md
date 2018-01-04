182138c02692cb61e293c13b596b90b51ba44171
stephenpardy <stephen.pardy@gmail.com>
2018-01-04 15:43:35 -0600

    Fixed names and more attribution

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Adding Magellanic Stream coordinates to astropy.
 
-Based on the coordinate system defined in Nidever et al. 2008 and on parameters provided by David Nidever.
+Based on the coordinate system defined in Nidever et al. 2008 and on parameters provided by David Nidever. Python code based on example for the [Sagitarius Stream](http://docs.astropy.org/en/stable/generated/examples/coordinates/plot_sgr-coordinate-frame.html).
 
-Currently a work in progress. Only Lambda and Beta work so far. Lots of work needs to be done to clean this up a bit and finish testing. 
+Currently a work in progress. Only Longitude and Latitude have been tested so far. Lots of work needs to be done to clean this up a bit and finish testing. 
--- magellanicstream/magellanicstream.py
+++ magellanicstream/magellanicstream.py
@@ -16,15 +16,15 @@ class MagellanicStream(coord.BaseCoordinateFrame):
     representation : `BaseRepresentation` or None
         A representation object or None to have no data (or use the other keywords)
 
-    Lambda : `Angle`, optional, must be keyword
+    MSLongitude : `Angle`, optional, must be keyword
         The longitude-like angle corresponding to the Magellanic Stream.
-    Beta : `Angle`, optional, must be keyword
+    MSLatitude : `Angle`, optional, must be keyword
         The latitude-like angle corresponding to the Magellanic Stream.
     distance : `Quantity`, optional, must be keyword
         The Distance for this object along the line-of-sight.
 
     pm_Lambda_cosBeta : :class:`~astropy.units.Quantity`, optional, must be keyword
-        The proper motion along the stream in ``Lambda`` (including the
+        The proper motion along the Stream in ``Lambda`` (including the
         ``cos(Beta)`` factor) for this object (``pm_Beta`` must also be given).
     pm_Beta : :class:`~astropy.units.Quantity`, optional, must be keyword
         The proper motion in Declination for this object (``pm_ra_cosdec`` must
@@ -38,16 +38,16 @@ class MagellanicStream(coord.BaseCoordinateFrame):
 
     frame_specific_representation_info = {
         coord.SphericalRepresentation: [
-            coord.RepresentationMapping('lon', 'Lambda'),
-            coord.RepresentationMapping('lat', 'Beta'),
+            coord.RepresentationMapping('lon', 'MSLongitude'),
+            coord.RepresentationMapping('lat', 'MSLatitude'),
             coord.RepresentationMapping('distance', 'distance')],
         coord.SphericalCosLatDifferential: [
-            coord.RepresentationMapping('d_lon_coslat', 'pm_Lambda_cosBeta'),
-            coord.RepresentationMapping('d_lat', 'pm_Beta'),
+            coord.RepresentationMapping('d_lon_coslat', 'pm_lon_coslat'),
+            coord.RepresentationMapping('d_lat', 'pm_lat'),
             coord.RepresentationMapping('d_distance', 'radial_velocity')],
         coord.SphericalDifferential: [
-            coord.RepresentationMapping('d_lon', 'pm_Lambda'),
-            coord.RepresentationMapping('d_lat', 'pm_Beta'),
+            coord.RepresentationMapping('d_lon', 'pm_lon'),
+            coord.RepresentationMapping('d_lat', 'pm_lat'),
             coord.RepresentationMapping('d_distance', 'radial_velocity')]
     }
 
@@ -63,7 +63,6 @@ MS_PHI = (180 + 8.5 + 90) * u.degree # Euler angles (from Nidever 2010)
 MS_THETA = (90 + 7.5) * u.degree
 MS_PSI = -32.724214217871349 * u.degree  # anode parameter from gal2mag.pro
 
-# Generate the rotation matrix using the x-convention (see Goldstein)
 D = rotation_matrix(MS_PHI, "z")
 C = rotation_matrix(MS_THETA, "x")
 B = rotation_matrix(MS_PSI, "z")
@@ -73,13 +72,13 @@ MS_MATRIX = matrix_product(A, B, C, D)
 @frame_transform_graph.transform(coord.StaticMatrixTransform, coord.Galactic, MagellanicStream)
 def galactic_to_MS():
     """ Compute the transformation matrix from Galactic spherical to
-        heliocentric Sgr coordinates.
+        Magellanic Stream coordinates.
     """
     return MS_MATRIX
 
 @frame_transform_graph.transform(coord.StaticMatrixTransform, MagellanicStream, coord.Galactic)
 def MS_to_galactic():
-    """ Compute the transformation matrix from heliocentric Sgr coordinates to
+    """ Compute the transformation matrix from Magellanic Stream coordinates to
         spherical Galactic.
     """
     return matrix_transpose(MS_MATRIX)
